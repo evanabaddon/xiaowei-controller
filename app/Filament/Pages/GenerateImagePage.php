35e0d047fe628ca9b0558e828dadd02b20b15909
evanhelgas <34735813+evanabaddon@users.noreply.github.com>
2025-07-09 07:58:26 +0700

    add flux base

--- app/Filament/Pages/GenerateImagePage.php
+++ app/Filament/Pages/GenerateImagePage.php
@@ -86,7 +86,7 @@ class GenerateImagePage extends Page implements Forms\Contracts\HasForms
 
             // Step 2: Generate Prompt from Gemini
             $gemini = new GeminiAIService();
-            $instruction = "kamu adalah AI yang membantu membuatkan prompt text to image dengan base model SDXL, jika saya berikan konteks, maka kamu akan membalas string 1 prompt text berbahasa inggris yang relevan dengan konteks nya. jika kamu akan membuat karakter, jaga agar tetap menghasilkan karakter orang Indonesia tanpa ada tambahan kalimat lainya, selalu gunakan nuansa Indonesia. Hindari menggunakan banyak karakter";
+            $instruction = "kamu adalah AI yang membantu membuatkan prompt text to image Model Flux1.Dev, jika saya berikan konteks, maka kamu akan membalas string 1 prompt text nya tanpa ada tambahan. selalu pertahankan hasil gambar khas Indonesia. contoh format prompt yang benar untuk konteks wanita cantik adalah 'Edge of a cliff overlooking a turbulent sea: Medieval Gothic oil painting by Roberto Ferri. Extreme close-up portrait of a Asian or Indonesian woman in a heavy, weathered cloak, standing at the cliff’s edge as the wind pulls at her long, dark hair. Her face is ghostly pale, with eyes that seem to reflect the restless waves below, filled with both longing and sorrow. The stormy sea and jagged rocks below amplify her haunting presence, as though she is forever bound to this desolate place. Cropped from upper chest to mid-forearm. [Composition: Tight 85mm lens effect, compress perspective to naturally exclude extremities.]'";
             $fullPrompt = "{$instruction}\n\nkonteks : {$this->prompt}";
 
             $generated = $gemini->generateGeminiResponse($fullPrompt);
